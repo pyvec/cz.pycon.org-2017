e9fe85836a03dd83cd446ee8d60557d929266491
starenka <starenka0@gmail.com>
2017-06-06 21:16:03 +0200

    simplify url

--- pyconcz_2017/intermission/urls.py
+++ pyconcz_2017/intermission/urls.py
@@ -4,7 +4,7 @@ from pyconcz_2017.intermission import views
 
 
 urlpatterns = [
-    url('^index/$', views.index, name='index'),
+    url('^$', views.index, name='cycle_all'),
     url('^sponsors/(?P<level>\d)+/$', views.sponsors, name='sponsors'),
     url('^annoucements/$', views.announcements, name='annoucements'),
     url('^up-next/(?P<track>\d+)/$', views.up_next, name='up_next'),
